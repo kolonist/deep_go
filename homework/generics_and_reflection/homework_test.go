a0e52560048e00c523ddb25418dba6d3812e63c6
Alexander Zubakov <kolonist@xinit.ru>
2026-02-14 20:12:33 +0300

    Serialization

--- homework/generics_and_reflection/homework_test.go
+++ homework/generics_and_reflection/homework_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,9 +18,40 @@ type Person struct {
 	Married bool   `properties:"married"`
 }
 
-func Serialize(person Person) string {
-	// need to implement
-	return ""
+// Никакой рекурсии или разыменования указателей не поддерживается, равно как не поддерживается
+// и сериализация примитивных типов
+func Serialize[T any](obj T) string {
+	const tagName string = "properties"
+	const omitEmptyTag string = "omitempty"
+
+	res := &strings.Builder{}
+
+	objValue := reflect.ValueOf(obj)
+
+	for field, val := range objValue.Fields() {
+		tag, ok := field.Tag.Lookup(tagName)
+		if !ok {
+			continue
+		}
+
+		parts := strings.Split(tag, ",")
+
+		if len(parts) > 1 && parts[1] == omitEmptyTag && val.IsZero() {
+			continue
+		}
+
+		strVal := fmt.Sprint(val)
+		strLen := len(parts[0]) + len(strVal) + 2
+
+		res.Grow(strLen)
+
+		res.WriteString(parts[0])
+		res.WriteString("=")
+		res.WriteString(strVal)
+		res.WriteString("\n")
+	}
+
+	return res.String()[:res.Len()-1]
 }
 
 func TestSerialization(t *testing.T) {
@@ -53,4 +87,55 @@ func TestSerialization(t *testing.T) {
 			assert.Equal(t, test.result, result)
 		})
 	}
+}
+
+func TestGenericSerialization(t *testing.T) {
+	type Struct1 struct {
+		F1 string   `properties:"f1"`
+		F2 int      `properties:"f2,omitempty"`
+		F3 []uint64 `properties:"f3"`
+		F4 *bool    `properties:"f4,omitempty"`
+	}
+
+	struct1 := Struct1{
+		F2: 0,
+		F3: []uint64{1, 2, 3},
+	}
+	expected1 := "f1=\nf3=[1 2 3]"
+
+	actual1 := Serialize(struct1)
+	assert.Equal(t, actual1, expected1)
+
+	type Struct2 struct {
+		F1 string  `properties:"f1,omitempty"`
+		F2 Struct1 `properties:"f2,omitempty"`
+	}
+
+	struct2 := Struct2{
+		F1: "kek",
+	}
+	expected2 := "f1=kek"
+
+	actual2 := Serialize(struct2)
+	assert.Equal(t, actual2, expected2)
+
+	struct2 = Struct2{
+		F1: "lol",
+		F2: Struct1{},
+	}
+	expected2 = "f1=lol"
+
+	actual2 = Serialize(struct2)
+	assert.Equal(t, actual2, expected2)
+
+	struct2 = Struct2{
+		F2: Struct1{
+			F2: 42,
+		},
+	}
+	expected2 = "f2={ 42 [] <nil>}"
+
+	actual2 = Serialize(struct2)
+	fmt.Println(actual2)
+	assert.Equal(t, actual2, expected2)
 }
